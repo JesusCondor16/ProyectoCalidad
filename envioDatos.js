4aff811fe99bd990e9263f30c48f97289b1f3715
JesusCondor16 <jesus.condor3@unmsm.edu.pe>
2023-05-28 14:49:04 -0500

    Actualizo documentos

--- envioDatos.js
+++ envioDatos.js
@@ -1,32 +1,69 @@
+document.getElementById('formulario.register').addEventListener('submit', function(event) {
+  event.preventDefault();
 
-<script src="RegistrarComprador.html"></script>
+  // Obtener los valores de los campos del formulario
+  const nombre = document.getElementById('nombre').value;
+  const apellido = document.getElementById('apellido').value;
+  const tipoDocumento = document.getElementById('tipoDocumento').value;
+  const documentNumber = document.getElementById('documentNumber').value;
+  const email = document.getElementById('email').value;
+  const contrasena = document.getElementById('contrasena').value;
+
+
+  // Crear un objeto con los datos del formulario
+  const formData = {
+    nombre: nombre,
+    apellido: apellido,
+    tipoDocumento:tipoDocumento,
+    documentNumber:documentNumber,
+    email: email,
+    contrasena: contrasena,
+  };
+
+
 document.getElementById('formulario.register').addEventListener('submit', function(event) {
     event.preventDefault();
-    const nombres = document.getElementById('nombresInput').value;
-    const apellidos = document.getElementById('apellidosInput').value;
-    const tipoDocumento = document.getElementById('tipoDocumentoInput').value;
-    const documentNumber = document.getElementById('documentNumberInput').value;
-    const email = document.getElementById('emailInput').value;
-    const password = document.getElementById('passwordInput').value;
-
-    fetch('/RegistrarComprador', {
+  
+    // Obtener los valores de los campos del formulario
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const tipoDocumento = document.getElementById('tipoDocumento').value;
+    const documentNumber = document.getElementById('documentNumber').value;
+    const email = document.getElementById('email').value;
+    const contrasena = document.getElementById('contrasena').value;
+  
+  
+    // Crear un objeto con los datos del formulario
+    const formData = {
+      nombre: nombre,
+      apellido: apellido,
+      tipoDocumento:tipoDocumento,
+      documentNumber:documentNumber,
+      email: email,
+      contrasena: contrasena,
+    };
+  
+    // Enviar los datos al back-end 
+    enviarDatosAlBackend(formData);
+  });
+  
+  function enviarDatosAlBackend(formData) {
+    fetch('recepcionDatos', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ email, password })
+      body: JSON.stringify(formData)
     })
-    .then(response => response.json())
-    .then(data => {
-      if (data.success) {
-        alert('Inicio de sesión exitoso');
-        // Realizar acciones adicionales para redireccionar o mostrar contenido adicional
-      } else {
-        alert('Inicio de sesión fallido: ' + data.message);
-      }
-    })
-    .catch(error => {
-      console.error('Error en la solicitud:', error);
-    });
-  });
+      .then(response => response.json())
+      .then(data => {
+        
+        console.log(data);
+        alert('Datos enviados correctamente');
+      })
+      .catch(error => {
+        console.error('Error en la solicitud:', error);
+        alert('Ocurrió un error al enviar los datos');
+      });
+  }
   
